9a9b4f8b18664dd6440664ebd8909694eef23d8a
Alex <xrlogic1@gmail.com>
2023-07-11 09:33:30 +0400

    *

---
+++ FileSystem/main.py
@@ -0,0 +1,28 @@
+# r - read
+# w - write
+# r+, w+ - read & write
+# a - append
+# a+ - append & read
+
+
+## list generators / list comprehesion
+# from random import randint
+#
+# values = [randint(0, 100) for i in range(10)]
+# evens = [i for i in values if i % 2 == 0]
+#
+# print(values)
+# print(evens)
+
+# fd = open("data.txt", 'w')  # truncate
+#
+# fd.write("Hello C++!")
+#
+#
+#
+#
+# names = ["John", "Rick", "Alex"]
+#
+# fd.writelines(names)
+#
+# fd.close()
